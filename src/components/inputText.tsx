8774fb44a4e3ad1643411e06797937223f5a117d
bbarado <bernardo.s.barardo@gmail.com>
2025-03-01 04:04:07 +0000

    Added main component

---
+++ src/components/inputText.tsx
@@ -0,0 +1,82 @@
+'use client'
+import React, { useState } from 'react';
+
+interface InputTextProps {
+}
+
+type LetterDict = { [key: string]: number };
+
+const InputText: React.FC<InputTextProps> = ({ }) => {
+    const [text, setText] = useState('');
+    const [words, setWords] = useState<string[]>([]);
+    const [wordsCode, setWordsCode] = useState<LetterDict>({});
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        event.preventDefault();
+        setText(event.target.value);
+        let words = event.target.value.split(' ');
+
+        setWords(words);
+        let letters = new Set(words.join(''));
+
+        let letterDict: LetterDict = wordsCode;
+        let availableNumbers = Array.from({ length: 101 }, (_, i) => i);
+
+        letters.forEach(letter => {
+            const randomIndex = Math.floor(Math.random() * availableNumbers.length);
+            letterDict[letter] = availableNumbers[randomIndex];
+            availableNumbers.splice(randomIndex, 1);
+        });
+
+        console.log(letterDict);
+        setWordsCode(letterDict);
+    };
+
+    return (
+        <div className="p-4 flex flex-col items-center">
+            <input
+                type="text"
+                value={text}
+                onChange={handleChange}
+                className="m-5 border border-gray-300 p-2 rounded w-full max-w-lg"
+                placeholder="Enter Text"
+            />
+            <table className="m-5 table-auto border-collapse border border-gray-400">
+                <thead>
+                    <tr>
+                        <th className="px-4 py-2 border border-gray-400">Word</th>
+                        {Object.keys(wordsCode).map((letter, index) => (
+                            <th key={index} className="border px-4 py-2 border-gray-400">{letter}</th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className="px-4 py-2 border border-gray-400">Code</td>
+                        {Object.keys(wordsCode).map((letter, index) => (
+                            <td key={index} className="border px-4 py-2 border-gray-400">{wordsCode[letter]}</td>
+                        ))}
+                    </tr>
+                </tbody>
+            </table>
+            <table className="m-5 table-auto border-collapse border border-gray-400">
+                <thead >
+                    <tr>
+                        <th className="px-4 py-2 border border-gray-400">Word</th>
+                        <th className="px-4 py-2 border border-gray-400">Code</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {words.map((word, index) => (
+                        <tr key={index}>
+                            <td className="px-4 py-2 border border-gray-400">{word}</td>
+                            <td className="px-4 py-2 border border-gray-400">{word.split('').map(letter => wordsCode[letter]).join(' ')}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default InputText;
