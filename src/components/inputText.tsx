667dc7ebaa0426f178fef2d037b873a1ea5d9fb8
bbarado <bernardo.s.barardo@gmail.com>
2025-03-06 01:34:57 +0000

    fixing a tailwind class

--- src/components/inputText.tsx
+++ src/components/inputText.tsx
@@ -115,7 +115,7 @@ const InputText: React.FC = ({ }) => {
                 OR
             </h1>
             <div className="flex items-center justify-center w-1/2 mb-3">
-                <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-32 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-32 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
                     <div className="flex flex-col items-center justify-center pt-5 pb-6">
                         <Image src={"/uploadFile.svg"} alt='upload file icon' width={50} height={50} />
                         <p className="mt-2 mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload </span>or drag and drop</p>
