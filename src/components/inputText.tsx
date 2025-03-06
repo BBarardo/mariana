7a66c3d33951b2fe933392d538269a075fcbaea1
bbarado <bernardo.s.barardo@gmail.com>
2025-03-06 01:32:38 +0000

    Added a way to read files

--- src/components/inputText.tsx
+++ src/components/inputText.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import ToPDF from './ToPDF';
 import type { LetterDict } from '@/types';
+import Image from 'next/image';
 
 const InputText: React.FC = ({ }) => {
     const [text, setText] = useState('');
@@ -27,6 +28,32 @@ const InputText: React.FC = ({ }) => {
 
         console.log(letterDict);
         setWordsCode(letterDict);
+    };
+
+    const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                const text = e.target?.result as string;
+                const words = text.split('\n').filter(word => word !== '').map(word => word.trim());
+                setWords(words);
+                setText(text);
+                const letters = new Set(words.join(''));
+
+                const letterDict: LetterDict = {};
+                const availableNumbers = Array.from({ length: 101 }, (_, i) => i);
+
+                letters.forEach(letter => {
+                    const randomIndex = Math.floor(Math.random() * availableNumbers.length);
+                    letterDict[letter] = availableNumbers[randomIndex];
+                    availableNumbers.splice(randomIndex, 1);
+                });
+
+                setWordsCode(letterDict);
+            };
+            reader.readAsText(file);
+        }
     };
 
     const reset = () => {
@@ -74,6 +101,9 @@ const InputText: React.FC = ({ }) => {
 
     return (
         <div className="p-4 flex flex-col items-center">
+            <h1 className="my-2 text-2xl font-bold text-center">
+                Write your each word on a new line
+            </h1>
             <textarea
                 rows={5}
                 value={text}
@@ -81,6 +111,24 @@ const InputText: React.FC = ({ }) => {
                 className="m-5 border border-gray-300 p-2 rounded w-full max-w-lg"
                 placeholder="Enter Text"
             />
+            <h1 className="mb-2 text-center">
+                OR
+            </h1>
+            <div className="flex items-center justify-center w-1/2 mb-3">
+                <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-32 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                    <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                        <Image src={"/uploadFile.svg"} alt='upload file icon' width={50} height={50} />
+                        <p className="mt-2 mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload </span>or drag and drop</p>
+                        <p className="text-xs text-gray-500 dark:text-gray-400">.txt</p>
+                    </div>
+                    <input
+                        className="hidden"
+                        id="dropzone-file"
+                        type="file"
+                        accept=".txt"
+                        onChange={handleFileUpload} />
+                </label>
+            </div>
             <Tables />
             <button type='button' onClick={reset} className="m-5 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">
                 Reset
